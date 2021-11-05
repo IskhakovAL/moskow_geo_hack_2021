b464cf606e5f579752a9dd4c0ffcac0d9b8ea1f0
Prokud1n <vaprokudin@edu.hse.ur>
2021-11-05 13:57:44 +0300

    добавил уникальный id

--- Frontend/src/services/MapService.tsx
+++ Frontend/src/services/MapService.tsx
@@ -3,7 +3,14 @@ import { RequestMethod } from '../client/BaseHttpClient';
 import { IFilterParams } from '../models/IFilterParams';
 import { IPositions } from '../models/IPositions';
 import { IDict } from '../models/IDict';
+import AuthUtils from '../utils/AuthUtils';
 
-export const fetchPositions = (params: IFilterParams) =>
-    Client.doRequest<IPositions>('locations', { method: RequestMethod.POST, data: params });
+export const fetchPositions = (params: IFilterParams) => {
+    const { login } = AuthUtils.getAuthMetadata();
+
+    return Client.doRequest<IPositions>(`locations/${login}`, {
+        method: RequestMethod.POST,
+        data: params,
+    });
+};
 export const fetchDict = () => Client.doRequest<IDict>('catalog');
