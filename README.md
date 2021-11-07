b6f57230269e02235bba0d6e3ac66e1340dcd94e
Георгий Рыбкин <rybkin.georgy@yandex.ru>
2021-11-07 08:12:00 +0300

    upd: change plots and readme.md

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # moskow_geo_hack_2021
 Проект для хакатона
 
-# Протитип
-62.84.123.182
+# Прототип
+http://62.84.123.182
 
 # Ссылка на презентацию
-https://drive.google.com/file/d/1IQqA35y23M_AleB0Kj03Pe2igdHL2gzF/view
+https://drive.google.com/file/d/1LrlG4mP3Jk60IIF3u95gSsg5vBObjCaS/view?usp=sharing
 
 # Ссылка на сопроводительную документацию
-https://drive.google.com/file/d/174xwFohRJdeR_XLxvcLs5wxCBtTj7_TM/view?usp=sharing
+https://drive.google.com/file/d/1Rfv-QREsotfq8QPldo_IXB72lMVHSXn3/view?usp=sharing
+
+# Ссылка на документацию Rest API
+https://documenter.getpostman.com/view/3750020/UV5ahGNC
--- app/views/extensions/rest_logic/plots.py
+++ app/views/extensions/rest_logic/plots.py
@@ -32,31 +32,34 @@ def generate_plots():
     sql_text = 'select * from plot2'
     data_people = pd.read_sql(sql_text, SQL_ENGINE)
     data_people = data_people.rename(columns={'NAME_AO': 'Район', 'sum': 'Количество'})
-    fig = px.pie(data_people, values='Количество', names='Район',
-                 title='Распределение численности населения по районам',
-                 hover_data=['Район'], labels={'Количество'})
-    fig.update_traces(textposition='inside', textinfo='percent+label')
+    fig = go.Figure()
+    fig.add_trace(go.Pie(values=data_people['Количество'], labels=data_people['Район']))
+    fig.update_layout(
+        title="Распределение численности населения по районам",
+        title_x=0.5,
+        margin=dict(l=0, r=0, t=30, b=0),
+        legend_orientation="h")
     plots.append(loads(dumps(fig, cls=PlotlyJSONEncoder)))
 
     # График 3
     sql_text = 'select * from plot3'
     data_zone = pd.read_sql(sql_text, SQL_ENGINE)
     data_zone = data_zone.rename(columns={'zones_type': 'Спортивная зона', 'cnt': 'Количество'})
-    fig = px.bar(data_zone, x='Спортивная зона', y='Количество')
+    fig = px.bar(data_zone, x='Спортивная зона', y = 'Количество', height=900, width=900)
     plots.append(loads(dumps(fig, cls=PlotlyJSONEncoder)))
 
     # График 4
     sql_text = 'select * from plot4'
     data_sport = pd.read_sql(sql_text, SQL_ENGINE)
     data_sport = data_sport.rename(columns={'sport_type': 'Вид спорта', 'cnt': 'Количество'})
-    fig = px.bar(data_sport, x='Вид спорта', y='Количество')
+    fig = px.bar(data_sport, x='Вид спорта', y = 'Количество', height=800, width=800)
     plots.append(loads(dumps(fig, cls=PlotlyJSONEncoder)))
 
     # График 5
     sql_text = 'select * from plot5'
     data_sport_type = pd.read_sql(sql_text, SQL_ENGINE)
     data_sport_type = data_sport_type.rename(columns={'sport_type': 'Вид спорта', 'med': 'Средняя площадь объектов'})
-    fig = px.bar(data_sport_type, x='Вид спорта', y='Средняя площадь объектов')
+    fig = px.bar(data_sport_type, x='Вид спорта', y = 'Средняя площадь объектов', height=900, width=900)
     plots.append(loads(dumps(fig, cls=PlotlyJSONEncoder)))
 
     # График 6
@@ -64,7 +67,7 @@ def generate_plots():
     data_zones_type = pd.read_sql(sql_text, SQL_ENGINE)
     data_zones_type = data_zones_type.rename(
         columns={'zones_type': 'Тип спортивной зоны', 'med': 'Средняя площадь спортивной зоны'})
-    fig = px.bar(data_zones_type, x='Тип спортивной зоны', y='Средняя площадь спортивной зоны')
+    fig = px.bar(data_zones_type, x='Тип спортивной зоны', y = 'Средняя площадь спортивной зоны', height=900, width=900)
     plots.append(loads(dumps(fig, cls=PlotlyJSONEncoder)))
 
     return plots
