564d00d1e2c28d05a5d8c7f0ef2896687597eff5
Георгий Рыбкин <rybkin.georgy@yandex.ru>
2022-01-28 18:29:00 +0300

    upd: fix ip

--- app/views/extensions/local_config.py
+++ app/views/extensions/local_config.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 
 # Конфиг с информацией об IP адресе сервера, на котором работает проект с созданием подключения к PSQL базе данных
-SERVER_ADDRESS = '62.84.123.182'
+SERVER_ADDRESS = '23.105.226.217'
 
 if os.name == 'posix':
     SQL_ENGINE = create_engine("postgresql://dbuser:qwerty12345@db:5432/db", pool_recycle=3600)
